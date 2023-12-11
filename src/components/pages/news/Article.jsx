248836a733b61d21e0552b90ba84992f628956db
gcpearse <glen.c.pearse@gmail.com>
2023-12-11 17:25:57 +0000

    further styling

--- src/components/pages/news/Article.jsx
+++ src/components/pages/news/Article.jsx
@@ -6,10 +6,10 @@ const Article = ({ article }) => {
   return (
     <div className="article">
       <li>
-        <p className="article-topic">Posted in <span id="article-topic-keyword">{article.topic}</span></p>
-        <h2 className="article-title">{article.title}</h2>
-        <img src={article.article_img_url} alt="An image representing the topic of the article" className="article-img" />
-        <p className="article-author-time">Published by {article.author} at {article.created_at.match(/\d{2}:\d{2}/)} on {lengthenDate(article.created_at.match(/\d{4}-\d{2}-\d{2}/)[0])}.</p>
+        <p id="article-topic">Posted in <span id="article-topic-keyword">{article.topic[0].toUpperCase() + article.topic.slice(1).toLowerCase()}</span></p>
+        <h2 id="article-title">{article.title}</h2>
+        <img src={article.article_img_url} alt="An image representing the topic of the article" id="article-img" />
+        <p className="article-author-time">Published by <b id="article-author-keyword">{article.author}</b> at {article.created_at.match(/\d{2}:\d{2}/)} on {lengthenDate(article.created_at.match(/\d{4}-\d{2}-\d{2}/)[0])}.</p>
         <div className="article-count-container">
           <p className="article-comment-count">{article.comment_count} comments</p>
           <p className="article-votes">{article.votes} {formatWord(article.votes)} { }</p>
