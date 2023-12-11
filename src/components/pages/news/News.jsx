6baf4e86988bc28bd60f8cc798b51fe251976df3
gcpearse <glen.c.pearse@gmail.com>
2023-12-11 14:28:08 +0000

    fundamental structure of News page with full list of articles

--- src/components/pages/news/News.jsx
+++ src/components/pages/news/News.jsx
@@ -1,6 +1,40 @@
+import { useEffect, useState } from "react";
+import { getArticles } from "../../../utils/api-utils";
+import Article from "./Article";
+
 const News = () => {
+
+  const [articles, setArticles] = useState([]);
+  const [isLoading, setIsLoading] = useState(true)
+  const [isError, setIsError] = useState(false)
+
+  useEffect(() => {
+    getArticles()
+      .then(({ articles }) => {
+        setArticles(articles);
+        setIsLoading(false)
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsLoading(false)
+        setIsError(true)
+      });
+  }, []);
+
+  if (isLoading) return <p>Loading content...</p>
+  if (isError) return <p>Oops! Something went wrong...</p>
+
   return (
-    <p>News test</p>
+    <section>
+      <div id="search-bar">
+        <p>Add sort / search / order tools here</p>
+      </div>
+      <ul id="articles-list">
+        {articles.map((article) => {
+          return <Article key={article.article_id} article={article} />;
+        })}
+      </ul>
+    </section>
   );
 };
 
