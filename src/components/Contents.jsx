5cdc83da004dabee7b50771baff3a1eab13a04bc
gcpearse <glen.c.pearse@gmail.com>
2023-12-11 15:41:38 +0000

    sets up route to single article page

--- src/components/Contents.jsx
+++ src/components/Contents.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import News from "./pages/news/News";
 import Topics from "./pages/topics/Topics";
+import SingleArticle from "./pages/article/SingleArticle";
 
 const Contents = () => {
   return (
@@ -9,6 +10,7 @@ const Contents = () => {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/news" element={<News />}></Route>
+        <Route path="/news/articles/:article_id" element={<SingleArticle />}></Route>
         <Route path="/topics" element={<Topics />}></Route>
       </Routes>
     </main>
