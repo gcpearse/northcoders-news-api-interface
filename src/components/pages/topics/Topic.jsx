18f9f2f99523f6ff05ae5705557303d80f4cbeba
gcpearse <glen.c.pearse@gmail.com>
2023-12-13 13:30:06 +0000

    links Topic components to News page with query params applied

--- src/components/pages/topics/Topic.jsx
+++ src/components/pages/topics/Topic.jsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom";
+
 const Topic = ({ topic }) => {
   return (
-    <li className="topic">
-      <h2 id="topic-header">{topic.slug[0].toUpperCase() + topic.slug.slice(1).toLowerCase()}</h2>
-      <p id="topic-body">{topic.description}</p>
-    </li>
+    <Link to={`/news?topic=${topic.slug}`} className="topic-link">
+      <li className="topic">
+        <h2 id="topic-header">{topic.slug[0].toUpperCase() + topic.slug.slice(1).toLowerCase()}</h2>
+        <p id="topic-body">{topic.description}</p>
+      </li>
+    </Link>
   );
 };
 
