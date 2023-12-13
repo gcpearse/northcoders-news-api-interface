aaaec166ac621fcf0ec0a6c7a92a505dcc036b00
gcpearse <glen.c.pearse@gmail.com>
2023-12-13 16:10:11 +0000

    moves state up to allow topic to be accessed in News component

--- src/components/Contents.jsx
+++ src/components/Contents.jsx
@@ -3,15 +3,19 @@ import Home from "./pages/home/Home";
 import News from "./pages/news/News";
 import Topics from "./pages/topics/Topics";
 import SingleArticle from "./pages/article/SingleArticle";
+import { useState } from "react";
 
 const Contents = () => {
+
+  const [topics, setTopics] = useState([]);
+
   return (
     <main>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/news" element={<News />}></Route>
         <Route path="/news/articles/:article_id" element={<SingleArticle />}></Route>
-        <Route path="/topics" element={<Topics />}></Route>
+        <Route path="/topics" element={<Topics topics={topics} setTopics={setTopics} />}></Route>
       </Routes>
     </main>
   );
