775a13d2e6a9581e4d334c3fd91507643c5a56b0
gcpearse <glen.c.pearse@gmail.com>
2023-12-13 17:00:14 +0000

    moves useEffect for getTopics up a level to ensure proper rendering in News

--- src/components/Contents.jsx
+++ src/components/Contents.jsx
@@ -3,19 +3,35 @@ import Home from "./pages/home/Home";
 import News from "./pages/news/News";
 import Topics from "./pages/topics/Topics";
 import SingleArticle from "./pages/article/SingleArticle";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { getTopics } from "../utils/api-utils";
 
 const Contents = () => {
 
   const [topics, setTopics] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    getTopics()
+      .then(({ topics }) => {
+        setTopics(topics);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsLoading(false);
+        setIsError(true);
+      });
+  }, []);
 
   return (
     <main>
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/news" element={<News />}></Route>
+        <Route path="/news" element={<News topics={topics} />}></Route>
         <Route path="/news/articles/:article_id" element={<SingleArticle />}></Route>
-        <Route path="/topics" element={<Topics topics={topics} setTopics={setTopics} />}></Route>
+        <Route path="/topics" element={<Topics topics={topics} setTopics={setTopics} isLoading={isLoading} isError={isError} />}></Route>
       </Routes>
     </main>
   );
