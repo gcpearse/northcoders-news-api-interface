d12d2aa1f170a106dd1c59f9e4916a49de1b0989
gcpearse <glen.c.pearse@gmail.com>
2023-12-13 10:38:06 +0000

    delete button is only visible to logged in viewers for comments they have posted

--- src/components/pages/article/Comment.jsx
+++ src/components/pages/article/Comment.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { formatWord, lengthenDate } from "../../../utils/formatting-utils";
 import { UserContext } from "../../../contexts/UserContext";
 import { deleteComment } from "../../../utils/api-utils";
@@ -6,13 +6,18 @@ import { deleteComment } from "../../../utils/api-utils";
 const Comment = ({ comment, toggle, setToggle }) => {
 
   const { user } = useContext(UserContext);
+  const [error, setError] = useState(null);
 
   const handleDelete = () => {
     if (user === comment.author) {
+      setError(null)
       deleteComment(comment.comment_id)
         .then(() => {
           setToggle(!toggle);
           console.log(toggle);
+        })
+        .catch(() => {
+          setError("Oops! Something went wrong...")
         });
     }
   };
@@ -24,12 +29,13 @@ const Comment = ({ comment, toggle, setToggle }) => {
       <p className="text-body">{comment.body}</p>
       <p id="comment-votes">{comment.votes} {formatWord(comment.votes)}</p>
       <div className="section-btns" id="comment-btns">
-        <button id="delete-comment-btn" onClick={handleDelete}>Delete</button>
+        {user === comment.author ? <button className="delete-comment-btn" onClick={handleDelete}>Delete</button> : <button disabled className="delete-comment-btn">Delete</button>}
         <div>
           <button className="vote-btn upvote-btn">+</button>
           <button className="vote-btn downvote-btn">-</button>
         </div>
       </div>
+      {error ? <p className="error">{error}</p> : null}
     </div>
   );
 };
