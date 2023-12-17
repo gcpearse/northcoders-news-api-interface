b4624fc46de26647aab810b12d4cfe193ccc2253
gcpearse <glen.c.pearse@gmail.com>
2023-12-17 20:44:23 +0000

    reorganisation of Topics page

--- src/components/pages/topics/Topic.jsx
+++ src/components/pages/topics/Topic.jsx
@@ -4,12 +4,19 @@ const Topic = ({ topic }) => {
   return (
     <li className="topic">
       <Link to={`/?topic=${topic.slug}`}>
-        <h2 id="topic-header">
+        <h3 id="topic-header">
           {topic.slug[0].toUpperCase() + topic.slug.slice(1).toLowerCase()}
-        </h2>
-        <p className="topic-body">{topic.description}</p>
-        <p className="topic-body">{topic.article_count} posts for this topic</p>
+        </h3>
+        <p className="topic-body">
+          {topic.description}
+        </p>
+        <p className="topic-body" id="topic-article-count">
+          {topic.article_count} articles for this topic
+        </p>
       </Link>
+      <button className="topic-body grey-btn">
+        Contribute
+      </button>
     </li>
   );
 };
