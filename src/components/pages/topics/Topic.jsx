5b980cf036cd482a4bfd1293bacfc2009e03cab1
gcpearse <glen.c.pearse@gmail.com>
2023-12-17 20:56:44 +0000

    removes nested button from Link component

--- src/components/pages/topics/Topic.jsx
+++ src/components/pages/topics/Topic.jsx
@@ -2,22 +2,24 @@ import { Link } from "react-router-dom";
 
 const Topic = ({ topic }) => {
   return (
-    <li className="topic">
-      <Link to={`/?topic=${topic.slug}`}>
-        <h3 id="topic-header">
-          {topic.slug[0].toUpperCase() + topic.slug.slice(1).toLowerCase()}
-        </h3>
-        <p className="topic-body">
-          {topic.description}
-        </p>
-        <p className="topic-body" id="topic-article-count">
-          {topic.article_count} articles for this topic
-        </p>
-      </Link>
-      <button className="topic-body grey-btn">
+    <div className="topic">
+      <li>
+        <Link to={`/?topic=${topic.slug}`}>
+          <h3 id="topic-header">
+            {topic.slug[0].toUpperCase() + topic.slug.slice(1).toLowerCase()}
+          </h3>
+          <p className="topic-body">
+            {topic.description}
+          </p>
+          <p className="topic-body" id="topic-article-count">
+            {topic.article_count} articles for this topic
+          </p>
+        </Link>
+      </li>
+      <button className="grey-btn" id="contribute-btn">
         Contribute
       </button>
-    </li>
+    </div>
   );
 };
 
